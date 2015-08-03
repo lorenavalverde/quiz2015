0a59e96d1ec5a5e0c6784da52252853d7e994235
Lorena <lorena.valverde@cgr.go.cr>
2015-08-03 08:46:38 -0600

    Validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,57 +1,78 @@
-var models = require('../models/models.js');
+ var models = require('../models/models.js');
  
-// Autoload - factoriza el código si ruta incluye :quizId
-exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId).then(
-    function(quiz) {
-      if (quiz) {
-        req.quiz = quiz;
-        next();
-      } else { next(new Error('No existe quizId=' + quizId)); }
-    }
-  ).catch(function(error) { next(error);});
-};
-
-// GET /quizes
-exports.index = function(req, res) {
-models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})};
- 
-
-// GET /quizes/:id
-exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
-};
-
-// GET /quizes/:id/answer
- exports.answer = function(req, res) {
-  var resultado = 'Incorrecto';
-  if (req.query.respuesta === req.quiz.respuesta) {
-    resultado = 'Correcto';
-  }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+ // Autoload - factoriza el código si ruta incluye :quizId
+ exports.load = function(req, res, next, quizId) {
+   models.Quiz.findById(quizId).then(
+     function(quiz) {
+       if (quiz) {
+         req.quiz = quiz;
+         next();
+      } else{next(new Error('No existe quizId=' + quizId))}
+     }
+  ).catch(function(error){next(error)});
  };
+ 
+ // GET /quizes
+ exports.index = function(req, res) {
+ //búsqueda
+   var inputValueSearch = (req.query.search || "texto_a_buscar");
+   var search = '%';
+   
+   if(req.query.search) {
+       search=search+req.query.search+'%';
+       search=search.replace(/\s+/g,'%');
+   }
+   models.Quiz.findAll(
+     { where: ["lower(pregunta) like lower(?)",search],
+       order: 'pregunta ASC'
+     } 
+   ).then(function(quizes){
+    res.render('quizes/index.ejs',{quizes: quizes, search: inputValueSearch, errors: []});
+  }).catch(function(error){next(error)});
+ };
+ 
+ 
+ // GET /quizes/:id
+ exports.show = function(req, res) {
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
+ };
+ 
+ // GET /quizes/:id/answer
+ exports.answer = function(req, res) {
+   var resultado = 'Incorrecto';
+   if (req.query.respuesta === req.quiz.respuesta) {
+     resultado = 'Correcto';
+   }
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
+ };
+ 
+ // GET /quizes/new
+ exports.new = function(req, res) {
+   var quiz = models.Quiz.build(
+     {pregunta: "Pregunta", respuesta: "Respuesta"}
+   );
+ 
+  res.render('quizes/new', {quiz: quiz, errors: []});
+ };
+ 
+ // POST /quizes/create
+ exports.create = function(req, res) {
+   var quiz = models.Quiz.build( req.body.quiz );
+ 
+ 
+ // guarda en DB los campos pregunta y respuesta de quiz
 
-
-
-// GET /quizes/new
-exports.new = function(req, res) {
-  var quiz = models.Quiz.build(
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
-  );
-
-  res.render('quizes/new', {quiz: quiz});
-};
-
-// POST /quizes/create
-exports.create = function(req, res) {
-  var quiz = models.Quiz.build( req.body.quiz );
-
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');  
-  })   // res.redirect: Redirección HTTP a lista de preguntas
-};
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+   }
+ );
+ };
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 
 // Página de entrada (home page)
  router.get('/', function(req, res) {
-   res.render('index', { title: 'Quiz' });
+    res.render('index', { title: 'Quiz', errors: []});
  });
  
 // Autoload de comandos con :quizId
